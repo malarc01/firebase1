5b3c6a6b1dc4b86082c3037da365c55e7618d8f7
Your Name <melky@Melkys-iMac.local>
2019-10-30 06:55:00 -0400

    add toggleClass for sign & signup

--- react-firebase/src/Login.jsx
+++ react-firebase/src/Login.jsx
@@ -4,21 +4,31 @@ import React, { Component } from 'react';
 import './login.css';
 
 class Login extends Component {
-	state = {};
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			active: false
+		};
+	}
+	toggleClass = (x) => {
+		console.log('SIGN UP CLICKED');
+		const currentState = this.state.active;
+		this.setState({ active: !currentState });
+	};
 
 	signInClick = (e) => {
-		let container = document.getElementById('container');
+		// e.preventDefault();
+		// let container = document.getElementById('container');
+		// container.classList.add('right-panel-active');
+		console.log('Sign In Event');
 	};
 
 	render() {
 		const { user } = this.props;
 
-		const signUpButton = document.getElementById('signUp');
-		const signInButton = document.getElementById('signIn');
-		const container = document.getElementById('container');
-
 		return (
-			<div className='container' id='container'>
+			<div className={this.state.active ? 'container right-panel-active' : 'container'} id='container'>
 				<div className='form-container sign-up-container'>
 					<form action='#'>
 						<h1>Create Account</h1>
@@ -36,7 +46,7 @@ class Login extends Component {
 							<input type='text' placeholder='Name' />
 							<input type='email' placeholder='Email' />
 							<input type='password' placeholder='Password' />
-							<button>Sign Up</button>
+							<button onClick={this.toggleClass}>Xign Up</button>
 						</div>
 					</form>
 				</div>
@@ -58,7 +68,7 @@ class Login extends Component {
 						<input type='email' placeholder='Email' />
 						<input type='password' placeholder='Password' />
 						<a href='facebook.com'>Forgot your password?</a>
-						<button onClick={this.signInClick()}>Sign In</button>
+						<button onClick={this.toggleClass}>Sign In</button>
 					</form>
 				</div>
 				<div className='overlay-container'>
@@ -66,14 +76,19 @@ class Login extends Component {
 						<div className='overlay-panel overlay-left'>
 							<h1>Welcome Back!</h1>
 							<p>To keep connected with us please login with your personal info</p>
-							<button className='ghost' id='signIn'>
-								Sign In
+							<button className='ghost' id='signIn' onClick={this.toggleClass}>
+								Gign In
 							</button>
 						</div>
 						<div className='overlay-panel overlay-right'>
 							<h1>Hello, Friend!</h1>
 							<p>Enter your personal details and start journey with us</p>
-							<button className='ghost' id='signUp'>
+							<button
+								className='ghost'
+								id='signUp'
+								onClick={this.toggleClass}
+								className={this.state.active ? 'ghost right-panel-active' : null}
+							>
 								Sign Up
 							</button>
 						</div>
